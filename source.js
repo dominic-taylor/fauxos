5fd34ab3b293f4c6e731a83dc900028fd93603ad
dominic-taylor <dataylorx@gmail.com>
2017-03-22 11:24:00 +1300

    Add react set up

--- source.js
+++ source.js
@@ -1,8 +1,8 @@
 document.onmousemove = moveEl;
 document.onmouseup = dropEl;
-
+change
 var icons = document.getElementsByClassName('icon')
-
+//fffr
 var selected = null, // Object of the element to be moved
     mouseX = 0, mouseY = 0, // Stores x & y coordinates of the mouse pointer
     elemX = 0, elemY = 0; 
