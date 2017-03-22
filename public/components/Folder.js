f93722f1bd750b6f30b3dda7f8a76e501d4bd726
dominic-taylor <dataylorx@gmail.com>
2017-03-23 10:37:40 +1300

    Basic click on folders working

--- public/components/Folder.js
+++ public/components/Folder.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-export default class Desktop extends React.Component {
-	
+export default class Folder extends React.Component {
+  clickHandler(){
+  	console.log('click: ', this.props.name)
+  }
   render() {
-    return	(<div className="icon" style={{top: this.props.top, left: this.props.left}} >
+    return	(<div className="icon" 
+    		 onClick={(e) => this.clickHandler(e)}
+    		 style={{top: this.props.top, left: this.props.left}} >
 				<div className="tab"></div>
 				<div className="foldergap"></div>
 				<div className="foldertitle"><p>{this.props.name}</p></div>
--- public/components/Icons.js
+++ public/components/Icons.js
@@ -2,38 +2,13 @@ import React from 'react';
 import Folder from './Folder';
 
 export default class Icons extends React.Component {
-  render() {return (
-  			<div>
-  				<Folder name="projects" left="5em" top="5em"/>
+	render() {
+  	return (<div>
+  				<Folder	name="projects" left="5em" top="5em" />
   				<Folder name="games" left="5em" top="12em" />	  
   				<Folder name="rubbish" left="5em" top="19em" />
   				<Folder name="contact" left="5em" top="26em" />
   				<Folder name="readme" left="15em" top="5em" />
-
 			</div>)
-			}
+	}
 }
-
-					// // <div className="icon" style={{left: '5em', top: '10em'}}>
-					// // 	<div className="tab"></div>
-					// // 	<div className="foldergap"></div>
-					// // 	<div className="foldertitle"><p>games</p></div>
-					// // </div>
-
-					// <div className="icon" style={{left: '5em', top: '15em'}}>
-					// 	<div className="tab"></div>
-					// 	<div className="foldergap"></div>
-					// 	<div className="foldertitle"><p>rubbish</p></div>
-					// </div>
-
-					// // <div className="icon" style={{left: '5em', top: '20em'}}>
-					// // 	<div className="tab"></div>
-					// // 	<div className="foldergap"></div>
-					// // 	<div className="foldertitle"><p>contact</p></div>
-					// // </div>
-
-					// // <div className="icon" style={{left: '10em', top:' 5em'}}>
-					// // 		<div className="tab"></div>
-					// // 		<div className="foldergap"></div>
-					// // 		<div className="foldertitle"><p>readme</p></div>
-					// </div>
