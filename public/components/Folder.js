fa6e4792758e8a527ea22ef9000fe1b72315cb6a
dominic-taylor <dataylorx@gmail.com>
2017-03-23 18:41:14 +1300

    Add panel class, drag working but not perfect

--- public/components/Folder.js
+++ public/components/Folder.js
@@ -1,16 +1,58 @@
 import React from 'react';
 
 export default class Folder extends React.Component {
-  clickHandler(){
-  	console.log('click: ', this.props.name)
+	
+  constructor(props) {
+    super(props);
+    this.state = {	mouseDown: false,
+	   				name: props.name, 
+	    			left: props.left,
+	    			top:props.top	
+	    		 };
+  }	
+
+  // mouse down -> grab coords and offset
+  //on mouse move, if mouse down true, make mouse coords 
+  // folder coords
+  // on mouse up change mouse down to false.(can't move)
+  mouseHandler(e){
+  	// console.log('handler: ', this.state)  	
+  	// let newLeft = e.clientX - e.nativeEvent.offsetX
+  	// let newTop = e.clientY - e.nativeEvent.offsetY
+
+  	this.setState({mouseDown: true})
+
+  	console.log(this.state)
   }
+  mouseMove(e){
+  	if(!this.state.mouseDown){
+  		return
+  	}else{
+  		console.log('mouse held and moving', e.target.left)
+  		
+  		let newLeft = e.clientX - 30 // cheap trick
+  	 	let newTop = e.clientY - 22
+  		this.setState({left: newLeft, top:newTop})
+  	}
+  }
+  mouseUp(e){
+  	// this needs to be attached to the body as well
+  	// as it means I have to let the mouse up over the div
+  	this.setState({mouseDown: false})
+  	console.log('mouse up')
+  }
+
+ 
+
   render() {
     return	(<div className="icon" 
-    		 onClick={(e) => this.clickHandler(e)}
-    		 style={{top: this.props.top, left: this.props.left}} >
+    		 onMouseDown={(e) => this.mouseHandler(e)}
+    		 onMouseMove={(e) => this.mouseMove(e)}
+    		 onMouseUp={(e) => this.mouseUp(e)}
+    		 style={{top: this.state.top, left: this.state.left}} >
 				<div className="tab"></div>
 				<div className="foldergap"></div>
-				<div className="foldertitle"><p>{this.props.name}</p></div>
+				<div className="foldertitle"><p>{this.state.name}</p></div>
 			</div>)
   }
 };
